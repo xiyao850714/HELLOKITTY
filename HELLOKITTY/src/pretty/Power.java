fe8b602051ff9a10123338d6261f4c4cd23183fc
onehao <onehao333@gmail.com>
2015-07-24 14:16:57 +0800

    add comments

--- HELLOKITTY/src/pretty/Power.java
+++ HELLOKITTY/src/pretty/Power.java
@@ -2,7 +2,7 @@ package pretty;
 public class Power {
 	public static double myPower(double base, int e) {
 		double result = 0.0d;
-		if (e == 0.0d)
+		if (e == 0.0d) // use e == 0, as e is int.
 			return 1.0d;
 		if (e == 1.0d)
 			return base;
@@ -11,15 +11,17 @@ public class Power {
 		result *= result;
 		if (e % 2 == 1.0d)
 			result *= base;
-		if (e > -0.000001 && e < 0.000001)
+		if (e > -0.000001 && e < 0.000001) //the intention of this?
 			result = 1.0 / result;
 		return result;
 	}
 
 	public static void main(String[] args) {
 		System.out.println(Power.myPower(5, 2));
+		//this use case not pass, as you are not dealing with the negative number, you can tryPower.myPower(0.5, -1)
 		System.out.println(Power.myPower(5, -1));
 		System.out.println(Power.myPower(0.0000000001, 2));
+		System.out.println(Power.myPower(0.5, -1));
 
 	}
 
