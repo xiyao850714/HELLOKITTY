d5c70d693f767bb2e86ae8de1103800f6404437f
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-07-24 14:58:55 +0800

    equal

--- HELLOKITTY/src/pretty/Power.java
+++ HELLOKITTY/src/pretty/Power.java
@@ -12,18 +12,27 @@ public class Power {
 		}
 		if (e % 2 == 1.0d)
 			result *= base;
-		if (e < 0) 
-//			if (base > -0.000001 && base < 0.000001)
+		if (e < 0 ) 
+			if (equal(base,0.0))
+				return 0.0d;
+			else
 				result = 1.0d / myPower(base, -e);			
 		return result;
 	}
 
+	public static boolean equal(double a, double b){
+		if((a-b) < 0.000001 && (a-b) > -0.000001)
+			return true;
+		else
+			return false;
+	}
 	public static void main(String[] args) {
 		System.out.println(Power.myPower(5, 2));
 		//this use case not pass, as you are not dealing with the negative number, you can tryPower.myPower(0.5, -1)
 		System.out.println(Power.myPower(5, 3));
 		System.out.println(Power.myPower(5, -2));
 		System.out.println(Power.myPower(0.0000000001, 2));
+		System.out.println(Power.myPower(0.0000000001, -2));
 		System.out.println(Power.myPower(0.5, -1));
 
 	}
