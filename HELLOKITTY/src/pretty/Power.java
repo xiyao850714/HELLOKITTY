e28bfac3d7e1138c48751a387c50cb508467bd41
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-07-24 14:22:48 +0800

    Modify e < 0

--- HELLOKITTY/src/pretty/Power.java
+++ HELLOKITTY/src/pretty/Power.java
@@ -6,19 +6,22 @@ public class Power {
 			return 1.0d;
 		if (e == 1.0d)
 			return base;
-		if (e > 1.0d)
+		if (e > 1.0d){
 			result = myPower(base, e >> 1);
-		result *= result;
+		    result *= result;			
+		}
 		if (e % 2 == 1.0d)
 			result *= base;
-		if (e > -0.000001 && e < 0.000001)
-			result = 1.0 / result;
+		if (e < 0) 
+//			if (base > -0.000001 && base < 0.000001)
+				result = 1.0d / myPower(base, -e);			
 		return result;
 	}
 
 	public static void main(String[] args) {
 		System.out.println(Power.myPower(5, 2));
-		System.out.println(Power.myPower(5, -1));
+		System.out.println(Power.myPower(5, 3));
+		System.out.println(Power.myPower(5, -2));
 		System.out.println(Power.myPower(0.0000000001, 2));
 
 	}
