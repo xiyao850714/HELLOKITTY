28089ac1386f5d7e9f8271e56c79194e2605089c
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-07-24 15:51:39 +0800

    Add <E>

--- HELLOKITTY/src/pretty/Listprinter.java
+++ HELLOKITTY/src/pretty/Listprinter.java
@@ -3,8 +3,8 @@ package pretty;
 import java.util.Stack;
 
 public class Listprinter {
-	public static void printRList(Node n){
-		Stack st = new Stack();
+	public static void printRList(Node<Integer> n){
+		Stack<Integer> st = new Stack<Integer>();
 		while (n != null){
 			st.push(n.data);
 //			System.out.print(st.peek());
@@ -17,9 +17,9 @@ public class Listprinter {
 	}
 
 	public static void main(String[] args) {
-	    Node nm1 = new Node(4,null);
-	    Node nm2 = new Node(1,nm1);
-	    Node nm3 = new Node(7,nm2);
+	    Node<Integer> nm1 = new Node<Integer>(4,null);
+	    Node<Integer> nm2 = new Node<Integer>(1,nm1);
+	    Node<Integer> nm3 = new Node<Integer>(7,nm2);
 	    Listprinter.printRList(nm3);
 	}
 
