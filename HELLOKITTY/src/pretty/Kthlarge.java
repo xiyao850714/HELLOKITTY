1bbbd8bb5f601f02349b0bb1ccef2342423e8810
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-07-28 13:14:52 +0800

    hello

---
+++ HELLOKITTY/src/pretty/Kthlarge.java
@@ -0,0 +1,14 @@
+package pretty;
+
+public class Kthlarge {
+	public static int findKthLargest(int[] n, int k){
+		
+		return k;		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
