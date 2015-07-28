ea81b52207e106f9c3baba93b8d9c7ea25f2521a
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-07-28 11:21:44 +0800

    again CQueue

--- HELLOKITTY/src/pretty/CQueue.java
+++ HELLOKITTY/src/pretty/CQueue.java
@@ -1,7 +1,44 @@
 package pretty;
 
+import java.util.Stack;
+
 public class CQueue{
-	public static void getQueue(){
-		
+	static Stack<Integer> stin = new Stack<Integer>();
+	static Stack<Integer> stout = new Stack<Integer>();
+	public static void queueIn(int n){
+		stin.push(n);
+		System.out.println(stin.peek());
+	}
+	public static int queueOut() throws Myexception{
+		if(stout.isEmpty() == false){
+			System.out.println(stout.peek());
+			return stout.pop();
+		}
+		if(stin.isEmpty() == true) {throw new Myexception("No elements in this queue");}
+		while(stin.isEmpty() == false){
+			stout.push(stin.pop());
+			System.out.println(stout.peek());
+		}
+		System.out.println(stout.peek());
+		return stout.pop();
+	}
+	public static void main(String[] args) {
+		try {
+			CQueue.queueOut();
+		} catch (Myexception e) {
+			System.out.println(e.getMessage());
+		}
+		CQueue.queueIn(5);
+		CQueue.queueIn(1);
+		CQueue.queueIn(8);		
+		try {
+			CQueue.queueOut();
+			CQueue.queueOut();
+			CQueue.queueOut();
+			CQueue.queueOut();
+		} catch (Myexception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 }
