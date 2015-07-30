de82d8f5ce59c05f8d62a919fd3b3c55b95fcb16
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-07-30 17:06:22 +0800

    Pattern, Matcher, appendedReplacement

--- HELLOKITTY/src/pretty/Space.java
+++ HELLOKITTY/src/pretty/Space.java
@@ -1,5 +1,8 @@
 package pretty;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Space {
 
 	public static void replaceSpace(String s){
@@ -9,6 +12,7 @@ public class Space {
 				System.out.print("%20");
 			else
 				System.out.print(c[i]);
+		System.out.println();
 		}
 	public static StringBuilder replaceSpace2(String s){
 		StringBuilder sb = new StringBuilder();
@@ -19,12 +23,29 @@ public class Space {
 			else
 				sb.append(c[i]);
 		System.out.print(sb);
+		System.out.println();
 		return sb;
 		}
+	public static StringBuffer replaceAll(String sb, String source, String target){
+        StringBuffer result = new StringBuffer();
+		Pattern p = Pattern.compile(source);
+		Matcher m = p.matcher(sb);
+		while(m.find()){
+			m.appendReplacement(result, target);		
+		}
+		m.appendTail(result);
+		System.out.println(result);
+		return result;
+		
+	}
 	public static void main(String[] args) {
+		String s = new String("We 1 We 2 We 3 We 4");
+		String s1 = new String("  We  1  %20  2");
 		Space.replaceSpace("   We are happy.");
 		Space.replaceSpace("We    are    happy.");
 		Space.replaceSpace2("   We are happy.");
 		Space.replaceSpace2("We    are    happy.");
+		Space.replaceAll(s, "We", "%20");
+		Space.replaceAll(s1, "%20", "We");
 	}
 }
