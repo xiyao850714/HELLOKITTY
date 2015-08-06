18b16cb073f513e42bfd812294cd6f92e5024a4d
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-08-06 16:31:01 +0800

    Seem ok

--- HELLOKITTY/src/pretty/Duplicatenum1.java
+++ HELLOKITTY/src/pretty/Duplicatenum1.java
@@ -2,40 +2,48 @@ package pretty;
 
 public class Duplicatenum1 {
 	public static Node<Integer> deleteDup(Node<Integer> n) throws Myexception{
-		if(n == null){
-			throw new Myexception("n should not be null");
-		}
-		if(n.next == null){
+		if(n == null && n.next == null){
 			return n;
 		}
-		Node<Integer> current = n;
-		Node<Integer> head = new Node<Integer>(0,current);
-		head.next = n;
-	    Node<Integer> temp = n;
-	    while(n != null && n.next != null){
-	    	temp = n.next;
-	    	if(n.data == n.next.data){
-	    		while(n.data == temp.data && temp.next != null){
-	    		temp = temp.next;
-	    	    }
-	    	    n = temp;
-	        }else{
-	        	current.next = n;
-	        	n = n.next;
-	        	current = current.next;
+		Node<Integer> head = new Node<Integer>(0,n);  
+	    Node<Integer> pre = head;  
+	    Node<Integer> cur = pre.next;  
+	    Node<Integer> next = cur.next;  
+	    //decide if it is dup
+	    boolean dup = false;        
+	           
+	    while(true){  
+	       if(next == null){  
+	           break;  
+	       }  
+	       if(cur.data != next.data){  
+	           if(dup){       
+	              pre.next = next;  
+	              dup = false;       
+	           }else{      
+	              pre = cur;  
+	           }  
+	           cur = next;  
+	           next = next.next;  
+	        }  
+	        else if(cur.data == next.data){  
+	                dup = true;  
+	                next = next.next;  
+	            }  
+	        }  
+	           
+	        if(dup){  
+	            pre.next = next;  
+	        }  
+	        head = head.next;
+	        while(head != null){
+	        	System.out.println(head.data);
+	        	head = head.next;
 	        }
-	    
-	    }
-	    current.next = n;
-	    head = head.next;
-	    while(head != null){
-	    	System.out.println(head.data);
-	    	head = head.next;
-	    }
-	    return head.next;
-	}
+	        return head.next;  
+	    }  
 	public static void main(String[] args) {
-		Node<Integer> micheal1 = new Node<Integer>(9, null);
+		Node<Integer> micheal1 = new Node<Integer>(8, null);
 		Node<Integer> micheal2 = new Node<Integer>(8, micheal1);
 		Node<Integer> micheal3 = new Node<Integer>(8, micheal2);
 		Node<Integer> michael4 = new Node<Integer>(7, micheal3);
@@ -48,12 +56,12 @@ public class Duplicatenum1 {
 		Node<Integer> michael11 = new Node<Integer>(3, michael10);
 		Node<Integer> michael12 = new Node<Integer>(3, michael11);
 		Node<Integer> michael13 = new Node<Integer>(2, michael12);
-		Node<Integer> michael14 = new Node<Integer>(1, michael13);
-		Node<Integer> michael15 = new Node<Integer>(1, michael14);
+		Node<Integer> michael14 = new Node<Integer>(2, michael13);
+		Node<Integer> michael15 = new Node<Integer>(2, michael14);
 		Node<Integer> michael16 = new Node<Integer>(1, michael15);
 		try {
 			Duplicatenum1.deleteDup(michael16);
-			Duplicatenum1.deleteDup(null);
+//			Duplicatenum1.deleteDup(null);
 		} catch (Myexception e) {
 			System.out.println(e.getMessage());
 		}
