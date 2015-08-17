6ebdb9f3e18aa6f2cb9c90eaa40715dac8b6b44e
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-08-17 16:32:29 +0800

    Treenode to TreeNode

--- HELLOKITTY/src/pretty/Treenode.java
+++ HELLOKITTY/src/pretty/Treenode.java
@@ -1,12 +1,12 @@
 package pretty;
 
-class Treenode<E> {
+class TreeNode<E> {
 		E data;
-		Treenode<E> left;
-		Treenode<E> right;
+		TreeNode<E> left;
+		TreeNode<E> right;
 
 		// testdfadsf
-		Treenode(E element, Treenode<E> left, Treenode<E> right) {
+		TreeNode(E element, TreeNode<E> left, TreeNode<E> right) {
 			this.data = element;
 			this.left = left;
 			this.right = right;
