0fcbf62868de9ec2ce4e8c9724e4f2b2fd2ac9b3
liyin <liyin@LIYIN-CN.cn.oracle.com>
2015-08-18 15:10:52 +0800

    Two stack realize one queue. Complete + add notes

--- HELLOKITTY/src/pretty/CQueue.java
+++ HELLOKITTY/src/pretty/CQueue.java
@@ -1,25 +1,32 @@
 package pretty;
 
 import java.util.Stack;
-
+/*
+ * Author: Linda.yin
+ * Use two Stack to realize Queue.
+ * Queue push & Queue pop.
+ * POP: if stout is not null, just pop.
+ *      if stout is null, pop all element in stin and push them to stout, then pop.
+ */
 public class CQueue{
 	static Stack<Integer> stin = new Stack<Integer>();
 	static Stack<Integer> stout = new Stack<Integer>();
+	
 	public static void queueIn(int n){
 		stin.push(n);
-		System.out.println(stin.peek());
+//		System.out.println(stin.peek());
 	}
 	public static int queueOut() throws Myexception{
-		if(stout.isEmpty() == false){
-			System.out.println(stout.peek());
+		if(!stout.isEmpty()){
+//			System.out.println(stout.peek());
 			return stout.pop();
 		}
-		if(stin.isEmpty() == true) {throw new Myexception("No elements in this queue");}
-		while(stin.isEmpty() == false){
+		if(stin.isEmpty()) {throw new Myexception("No elements in this queue");}
+		while(!stin.isEmpty()){
 			stout.push(stin.pop());
-			System.out.println(stout.peek());
+//			System.out.println(stout.peek());
 		}
-		System.out.println(stout.peek());
+//		System.out.println(stout.peek());
 		return stout.pop();
 	}
 	public static void main(String[] args) {
